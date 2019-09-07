b180cc93a442d8a691dbc9f2d71fa727113aa359
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-07 23:12:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,22 @@
 # Write your code here!
-
-
+def game_hash(teams)
+  teams = {
+    home:{
+      :team_name => [],
+      :colors => [],
+      :players =>{
+        numbers: 
+        shoe:
+        points:
+        rebounds:
+        assists:
+        steals:
+        blocks:
+        slam_dunks:
+      }
+    }
+  }
+end 
 
 
 
