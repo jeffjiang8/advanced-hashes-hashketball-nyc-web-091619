4889a368b07d77edebd28f785d5df67dc241473b
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-07 23:20:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -49,8 +49,9 @@ def game_hash(teams)
          assists: "2",
          steals: "4",
          blocks: "11",
-         slam_dunks: "1"
-    }
+         slam_dunks: "1"}]
+         }
+    away:
   }
 end 
 
