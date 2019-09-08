b8e50fca0088381df3a5cf6c7becdbbfd1b087c5
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-08 00:33:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,5 +200,28 @@ def big_shoe_rebounds
   num_rebounds
 end 
 
+def player_with_most_of (statistic)
+  player_name = nil 
+  amount_of_stat = 0 
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      if player[statistic].is_a? String
+        if player[statistic].length > amount_of_stat
+          amount_of_stat = player[statistic].length
+          player_name = player[:player_name]
+        end 
+        elsif player[statistic] > amount_of_stat
+        amount_of_stat = player[statistic]
+        player_name = player[:player_name]
+      end 
+    end 
+  end 
+  player_name
+end 
+
+def most_points_scored
+  player_with_most_of(:points)
+end 
+
 
 
