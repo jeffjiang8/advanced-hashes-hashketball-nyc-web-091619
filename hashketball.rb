e988d715a1632ce33162193d27ddc476dab9b65f
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-08 00:06:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -140,7 +140,18 @@ def team_names
 end
 
 def player_numbers (team_name)
-  
+  nums = []
+  game_hash.each do |_place, team|
+    next unless team[:team_name] == team_name
+    team.each do |attribute, data|
+      next unless attribute == :players
+      data.each do |data|
+        nums.push(data[:number])
+      end 
+    end
+  end 
+  nums
+end 
 
 
 
