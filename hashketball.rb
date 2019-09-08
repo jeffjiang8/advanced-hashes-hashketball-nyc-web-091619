1f8e50b790010f142146de742c4d9b906eca6cf7
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-09-08 00:41:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -226,6 +226,7 @@ end
 def winning_team
   scores = {"Brooklyn Nets" => 0, "Charlotte Hornets" => 0}
   game_hash.each do |_team, game_data|
+    game_data
     scores[game_data[:team_name]] += iterate_thru_players(player[:player_name], :points)
   end 
 end 
